260eafc0f10e9fbc627ad29f348f27ecff0aa83c
wizand0 <xabor@yandex.ru>
2025-05-12 17:12:24 +0300

    view binding

--- app/src/main/java/ru/wizand/cryptoapp/presentation/CoinDetailActivity.kt
+++ app/src/main/java/ru/wizand/cryptoapp/presentation/CoinDetailActivity.kt
@@ -3,20 +3,23 @@ package ru.wizand.cryptoapp.presentation
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.example.cryptoapp.R
+import com.example.cryptoapp.databinding.ActivityCoinDetailBinding
 import com.squareup.picasso.Picasso
 
 class CoinDetailActivity : AppCompatActivity() {
 
     private lateinit var viewModel: CoinViewModel
 
+    private val binding by lazy {
+        ActivityCoinDetailBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coin_detail)
+//        setContentView(R.layout.activity_coin_detail)
+        setContentView(binding.root)
         if (!intent.hasExtra(EXTRA_FROM_SYMBOL)) {
             finish()
             return
@@ -24,24 +27,26 @@ class CoinDetailActivity : AppCompatActivity() {
         val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
 
-        val tvPrice = findViewById<TextView>(R.id.tvPrice)
-        val tvMinPrice = findViewById<TextView>(R.id.tvMinPrice)
-        val tvMaxPrice = findViewById<TextView>(R.id.tvMaxPrice)
-        val tvLastMarket = findViewById<TextView>(R.id.tvLastMarket)
-        val tvLastUpdate = findViewById<TextView>(R.id.tvLastUpdate)
-        val tvFromSymbol = findViewById<TextView>(R.id.tvFromSymbol)
-        val tvToSymbol = findViewById<TextView>(R.id.tvToSymbol)
-        val ivLogoCoin = findViewById<ImageView>(R.id.ivLogoCoin)
+//        val tvPrice = findViewById<TextView>(R.id.tvPrice)
+//        val tvMinPrice = findViewById<TextView>(R.id.tvMinPrice)
+//        val tvMaxPrice = findViewById<TextView>(R.id.tvMaxPrice)
+//        val tvLastMarket = findViewById<TextView>(R.id.tvLastMarket)
+//        val tvLastUpdate = findViewById<TextView>(R.id.tvLastUpdate)
+//        val tvFromSymbol = findViewById<TextView>(R.id.tvFromSymbol)
+//        val tvToSymbol = findViewById<TextView>(R.id.tvToSymbol)
+//        val ivLogoCoin = findViewById<ImageView>(R.id.ivLogoCoin)
 
         viewModel.getDetailInfo(fromSymbol).observe(this) {
-            tvPrice.text = it.price
-            tvMinPrice.text = it.lowDay
-            tvMaxPrice.text = it.highDay
-            tvLastMarket.text = it.lastMarket
-            tvLastUpdate.text = it.lastUpdate
-            tvFromSymbol.text = it.fromSymbol
-            tvToSymbol.text = it.toSymbol
-            Picasso.get().load(it.imageUrl).into(ivLogoCoin)
+            with(binding) {
+                tvPrice.text = it.price
+                tvMinPrice.text = it.lowDay
+                tvMaxPrice.text = it.highDay
+                tvLastMarket.text = it.lastMarket
+                tvLastUpdate.text = it.lastUpdate
+                tvFromSymbol.text = it.fromSymbol
+                tvToSymbol.text = it.toSymbol
+                Picasso.get().load(it.imageUrl).into(ivLogoCoin)
+            }
         }
     }
 
--- app/src/main/java/ru/wizand/cryptoapp/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/ru/wizand/cryptoapp/presentation/CoinPriceListActivity.kt
@@ -2,21 +2,24 @@ package ru.wizand.cryptoapp.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cryptoapp.R
-import ru.wizand.cryptoapp.presentation.adapters.CoinInfoAdapter
-import ru.wizand.cryptoapp.data.network.model.CoinInfoDto
+import com.example.cryptoapp.databinding.ActivityCoinPriceListBinding
 import ru.wizand.cryptoapp.domain.CoinInfo
+import ru.wizand.cryptoapp.presentation.adapters.CoinInfoAdapter
 
 class CoinPriceListActivity : AppCompatActivity() {
 
     private lateinit var viewModel: CoinViewModel
 
+    private val binding by lazy {
+        ActivityCoinPriceListBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coin_prce_list)
+        setContentView(binding.root)
         val adapter = CoinInfoAdapter(this)
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinPriceInfo: CoinInfo) {
@@ -27,8 +30,8 @@ class CoinPriceListActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
-        val rvCoinPriceList = findViewById<RecyclerView>(R.id.rvCoinPriceList)
-        rvCoinPriceList.adapter = adapter
+//        val rvCoinPriceList = findViewById<RecyclerView>(R.id.rvCoinPriceList)
+        binding.rvCoinPriceList.adapter = adapter
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
         viewModel.coinInfoList.observe(this) {
             adapter.coinInfoList = it
